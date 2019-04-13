40dd6a587d8357d28ed229367f947d879ca2fdff
Buhobit <byron.lopez.begins@gmail.com>
2019-04-12 22:57:24 -0500

    5. Variables let y const

--- app.ts
+++ app.ts
@@ -1,11 +1,13 @@
 var mensaje = "Hola";
-var miNombre = "Buhobit";
-var anio = 2019;
+let miNombre = "Buhobit";
+const anio = 2019;
+
 if (true) {
-    var mensaje = "adios";
-    var miNombre_1 = "Brandom";
-    var anio_1 = 2018;
+  var mensaje = "adios";
+  let miNombre = "Brandom";
+  const anio = 2018;
 }
+
 //Imprime adios
 console.log(mensaje);
 //imprime Buhobit
