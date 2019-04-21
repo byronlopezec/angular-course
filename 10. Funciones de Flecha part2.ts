9384cd0584cee7e744db8e985e5bdb01f34dda80
Buhobit <byron.lopez.begins@gmail.com>
2019-04-20 22:51:47 -0500

    10. Funciones de Flecha nuevo guardado!

---
+++ 10. Funciones de Flecha part2.ts
@@ -0,0 +1,22 @@
+let hulk = {
+  nombre: "Hulk",
+  smash() {
+    console.log(this.nombre + " smash!!");
+  }
+};
+let hulkFlecha = {
+  nombre: "Hulk",
+  smash() {
+    //Cuando se ejecuta un setTimeout el this apunta el windows o variable global,
+    //lo resuelvo usando función de flecha
+    setTimeout(function() {
+      console.log(this.nombre + " smash!!");
+    }, 2000);
+    setTimeout(() => {
+      console.log(this.nombre + " smash!!");
+    }, 2000);
+  }
+};
+
+hulk.smash();
+hulkFlecha.smash();
