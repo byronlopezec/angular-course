a65b80328a23706a362629e325ac83f4bd998f68
Buhobit <byron.lopez.begins@gmail.com>
2019-04-25 11:49:35 -0500

    11. Creando el footer.component

--- 01-hola-mundo/src/app/app.module.ts
+++ 01-hola-mundo/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { BodyComponent } from "./components/body/body.component";
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, BodyComponent],
+  declarations: [AppComponent, HeaderComponent, BodyComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
