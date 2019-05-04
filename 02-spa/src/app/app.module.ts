ceb773b9d8c3aba016da7e3aeb4ffd5cd9329fcf
Buhobit <byron.lopez.begins@gmail.com>
2019-05-04 11:46:17 -0500

    7. Configurando el navbar y otros componentes

--- 02-spa/src/app/app.module.ts
+++ 02-spa/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { NavbarComponent } from "./components/navbar/navbar.component";
+import { NavbarComponent } from "./components/shared/navbar/navbar.component";
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent,HomeComponent, AboutComponent, HeroesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
