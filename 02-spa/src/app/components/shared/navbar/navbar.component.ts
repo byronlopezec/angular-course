6ab4a9fce5d55b53b9bbfc219dd74dfa05784c6f
Buhobit <byron.lopez.begins@gmail.com>
2019-05-06 10:23:12 -0500

    19. Buscador de Héroes

--- 02-spa/src/app/components/shared/navbar/navbar.component.ts
+++ 02-spa/src/app/components/shared/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from "@angular/core";
+import { HeroesService } from "../../../services/heroes.service";
 
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html"
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  constructor(private _heroesService: HeroesService) {}
 
   ngOnInit() {}
+
+  buscarHeroe(termino: string) {
+    console.log(this._heroesService.encontrarHeroes(termino));
+  }
 }
