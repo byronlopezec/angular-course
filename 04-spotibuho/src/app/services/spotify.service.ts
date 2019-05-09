b7d76ebe219d9ae94df7adfc25183ca78f216a21
Buhobit <byron.lopez.begins@gmail.com>
2019-05-09 12:56:32 -0500

    13. Centralizar las peticiones hacia Spotify

--- 04-spotibuho/src/app/services/spotify.service.ts
+++ 04-spotibuho/src/app/services/spotify.service.ts
@@ -9,36 +9,33 @@ import { map } from "rxjs/operators";
 export class SpotifyService {
   constructor(private http: HttpClient) {}
 
-  getNewReleases() {
+  getQuery(ruta: string) {
+    const url = `https://api.spotify.com/v1/${ruta}`;
+
     const headers = new HttpHeaders({
       Authorization:
         "Bearer " +
-        "BQCIWb-9SFDdlDJUhnnvQxRYlV0fB_69Nj-Xs8CDG2QeD1UW8robNytR-CIqH21vUPSbNYCU7OE2N0k7Mvc"
+        "BQBIWSKdg08lG42qPtNNGBzdqWOLxH2eq4nKIilb02I1yeQBAxVrav79sekzNmEoOdM0k-rrnrlV3FzuN9k"
     });
-    return this.http
-      .get("https://api.spotify.com/v1/browse/new-releases", {
-        headers
+    return this.http.get(url, { headers });
+  }
+
+  getNewReleases() {
+    return this.getQuery("browse/new-releases").pipe(
+      map((data: any) => {
+        console.log(data);
+        return data.albums.items;
       })
-      .pipe(
-        map((data: any) => {
-          console.log(data);
-          return data.albums.items;
-        })
-      );
+    );
   }
 
   getArtists(termino: string) {
-    const headers = new HttpHeaders({
-      Authorization:
-        "Bearer " +
-        "BQCIWb-9SFDdlDJUhnnvQxRYlV0fB_69Nj-Xs8CDG2QeD1UW8robNytR-CIqH21vUPSbNYCU7OE2N0k7Mvc"
-    });
-
-    return this.http.get(
-      `https://api.spotify.com/v1/search?query=${termino}&type=artist&market=EC&offset=5&limit=15`,
-      { headers }
-    ).pipe(map((data: any) => {
-      return data.artists.items;
-    }));
+    return this.getQuery(
+      `search?query=${termino}&type=artist&market=EC&offset=5&limit=15`
+    ).pipe(
+      map((data: any) => {
+        return data.artists.items;
+      })
+    );
   }
 }
