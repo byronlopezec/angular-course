910072e0e74a7771b9054c46509288292262d15d
Buhobit <byron.lopez.begins@gmail.com>
2019-05-13 23:07:17 -0500

    8. Usando procesos asíncronos con indicadores de usuario

--- 06-miscelaneos/src/app/components/clases/clases.component.ts
+++ 06-miscelaneos/src/app/components/clases/clases.component.ts
@@ -7,13 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClasesComponent implements OnInit {
   alerta: string;
-
-  propiedades: Object = {
+  loading: boolean;
+  propiedades: any = {
     danger: false
   };
   constructor() {
     this.alerta = 'alert-success';
+    this.loading = false;
   }
 
   ngOnInit() {}
+
+  ejecutar() {
+    this.loading = true;
+
+    setTimeout(() => {
+      this.loading = false;
+    }, 3000);
+  }
 }
