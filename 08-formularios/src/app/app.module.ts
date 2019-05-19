0c30cdd6aa3dcdb21af41842521b1b9a75dca2dc
Buhobit <byron.lopez.begins@gmail.com>
2019-05-18 19:47:13 -0500

    6. Creación de componentes - Aproximación por Template y Data

--- 08-formularios/src/app/app.module.ts
+++ 08-formularios/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataComponent } from './components/data/data.component';
+import { TemplateComponent } from './components/template/template.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DataComponent, TemplateComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 08-formularios/src/app/components/template/template.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
