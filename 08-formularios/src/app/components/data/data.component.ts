74611db15f587409fe883fdb04a8c6f4be516c59
Buhobit <byron.lopez.begins@gmail.com>
2019-05-20 13:41:46 -0500

    17. Data Continuando con las validaciones y HTML

--- 08-formularios/src/app/components/data/data.component.ts
+++ 08-formularios/src/app/components/data/data.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { stringify } from '@angular/core/src/render3/util';
 
 @Component({
   selector: 'app-data',
@@ -11,7 +12,7 @@ export class DataComponent {
 
   constructor() {
     this.forma = new FormGroup({
-      nombre: new FormControl('', Validators.required),
+      nombre: new FormControl('', [Validators.required, Validators.minLength(5)]),
       apellido: new FormControl('', Validators.required),
       correo: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')]),
     });
@@ -20,5 +21,33 @@ export class DataComponent {
   guardar() {
     console.log(': DataComponent -> formGroup', this.forma.value);
     console.log(': DataComponent -> formGroup', this.forma);
+  }
+  isInvalid(entrada: string): string {
+    const isTouched = this.forma.controls[entrada].touched;
+    const error = this.forma.controls[entrada].errors;
+    if (!isTouched || error === null) {
+      return '';
+    } else {
+      const tipo = Object.keys(error) + '';
+      return this.getMessageError(tipo);
+    }
+  }
+  isValid(entrada: string) {
+    const isTouched = this.forma.controls[entrada].touched;
+    const error = this.forma.controls[entrada].errors;
+    return isTouched && !error;
+  }
+
+  getMessageError(tipo: string): string {
+    if (tipo === 'required') {
+      return 'Campo requerido';
+    }
+    if (tipo === 'minlength') {
+      return 'Se permiten minimo 5 caracteres';
+    }
+    if (tipo === 'pattern') {
+      return 'Correo no encontrado';
+    }
+    return '';
   }
 }
