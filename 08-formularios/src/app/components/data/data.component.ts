3a6839704b3b9052202a101afd9a1c8f03ccad9e
Buhobit <byron.lopez.begins@gmail.com>
2019-05-20 11:04:23 -0500

    14. Data Aproximación de formularios utilizando código

--- 08-formularios/src/app/components/data/data.component.ts
+++ 08-formularios/src/app/components/data/data.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
-  styleUrls: ['./data.component.css']
+  styleUrls: ['./data.component.css'],
 })
 export class DataComponent implements OnInit {
+  usuario: object;
+  paises: object;
+  sexo: string[];
 
-  constructor() { }
+  constructor() {
+    this.usuario = {
+      nombre: '',
+      apellido: '',
+      correo: '',
+      pais: '',
+      sexo: 'Masculino',
+      acepta: false,
+    };
+    this.sexo = ['Masculino', 'Femenino', 'Sin Definir'];
 
-  ngOnInit() {
+    this.paises = [
+      {
+        codigo: 'es',
+        nombre: 'España',
+      },
+      {
+        codigo: 'ec',
+        nombre: 'Ecuador',
+      },
+      {
+        codigo: 'us',
+        nombre: 'Estados Unidos',
+      },
+    ];
   }
 
+  ngOnInit() {}
+
+  guardar(forma: NgForm) {
+    console.log('TemplateComponent -> guardar -> forma', forma);
+    console.log('Out: TemplateComponent -> guardar -> forma.value', forma.value);
+    console.log('Out: TemplateComponent -> usuario', this.usuario);
+  }
 }
