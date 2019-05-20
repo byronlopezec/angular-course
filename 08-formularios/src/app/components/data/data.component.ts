6819b46a23c20a9b6616690466886329d7f82e20
Buhobit <byron.lopez.begins@gmail.com>
2019-05-20 13:43:29 -0500

    17. Data Continuando con las validaciones y HTML

--- 08-formularios/src/app/components/data/data.component.ts
+++ 08-formularios/src/app/components/data/data.component.ts
@@ -13,7 +13,7 @@ export class DataComponent {
   constructor() {
     this.forma = new FormGroup({
       nombre: new FormControl('', [Validators.required, Validators.minLength(5)]),
-      apellido: new FormControl('', Validators.required),
+      apellido: new FormControl('', [Validators.required, Validators.minLength(5)]),
       correo: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')]),
     });
   }
