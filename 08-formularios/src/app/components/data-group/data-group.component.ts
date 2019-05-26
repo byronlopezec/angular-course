cf407590a1428cc359fe0212a2cca0eebbca9383
Buhobit <byron.lopez.begins@gmail.com>
2019-05-25 20:15:56 -0500

    23. Data Validadores asíncronos

--- 08-formularios/src/app/components/data-group/data-group.component.ts
+++ 08-formularios/src/app/components/data-group/data-group.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-data-group',
@@ -26,12 +27,27 @@ export class DataGroupComponent {
       }),
       correo: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')]),
       pasaTiempos: new FormArray([new FormControl('', [Validators.required])]),
+      usuario: new FormControl('', Validators.required, this.existeUsuario),
       password1: new FormControl('', Validators.required),
       password2: new FormControl(),
       // password2: new FormControl('', [this.noiguales.bind(this.forma)]),
     });
 
     this.forma.get('password2').setValidators([Validators.required, this.noiguales.bind(this.forma)]);
+  }
+
+  existeUsuario(control: FormControl): Promise<any> | Observable<any> {
+    const promesa = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'buhobit') {
+          resolve({ yaexisiteusuario: true });
+        } else {
+          resolve(null);
+        }
+      }, 3000);
+    });
+
+    return promesa;
   }
 
   noiguales(control: FormControl): { [s: string]: boolean } {
@@ -68,11 +84,12 @@ export class DataGroupComponent {
   }
 
   isInvalid(control: FormControl): string {
-    if (control.untouched || control.valid) {
+    if (control.untouched || control.valid || !control.errors) {
       return '';
+    } else {
+      const tipo = Object.keys(control.errors)[0] + '';
+      return this.getMessageError(tipo);
     }
-    const tipo = Object.keys(control.errors)[0] + '';
-    return this.getMessageError(tipo);
   }
 
   isValid(control: FormControl) {
@@ -80,6 +97,9 @@ export class DataGroupComponent {
   }
 
   getMessageError(tipo: string): string {
+    if (tipo === 'yaexisiteusuario') {
+      return 'El usuario ya se ecuentra registrado!';
+    }
     if (tipo === 'required') {
       return 'Campo requerido';
     }
