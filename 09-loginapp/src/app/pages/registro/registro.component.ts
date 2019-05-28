48ff1b41acb44802d02d4f4dc826f592ed4f31db
Buhobit <byron.lopez.begins@gmail.com>
2019-05-28 10:22:43 -0500

    6. Validar la información antes de enviarla a un servidor

--- 09-loginapp/src/app/pages/registro/registro.component.ts
+++ 09-loginapp/src/app/pages/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from 'src/app/models/usuario.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -16,8 +17,12 @@ export class RegistroComponent implements OnInit {
     this.usuario.email = 'buhobit@gmail.com';
   }
 
-  onSubmit() {
+  onSubmit(forma: NgForm) {
+    if (forma.invalid) {
+      return;
+    }
     console.log('Formulario enviado!');
     console.log(this.usuario);
+    console.log(forma);
   }
 }
