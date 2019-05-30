9d7768c9789c8b31dc5abdf0ab34cd1e179ea563
Buhobit <byron.lopez.begins@gmail.com>
2019-05-29 19:02:23 -0500

    5. Inicio del proyecto - CRUD - Firebase

---
+++ 10-heroesapp/.prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  proseWrap: 'always',
+  jsxBracketSameLine: true,
+  useTabs: false,
+  quoteProps: 'as-needed',
+  // tabWidth: 2,
+};
