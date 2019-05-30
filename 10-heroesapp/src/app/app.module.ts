dc8e1adbaf22e4f9594779084808dba16985dc13
Buhobit <byron.lopez.begins@gmail.com>
2019-05-29 19:16:48 -0500

    6. Creando los componentes y rutas de la aplicación

--- 10-heroesapp/src/app/app.module.ts
+++ 10-heroesapp/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroes/heroe.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeroesComponent, HeroeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ 10-heroesapp/src/app/components/heroes/heroe.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styles: [],
+})
+export class HeroeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
